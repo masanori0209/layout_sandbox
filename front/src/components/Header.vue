51a85aa4d867d2c37266b6a1bc3728a13d5842ac
masanori0209 <masanori.murakoshi@akarie.co.jp>
2021-06-10 08:17:45 +0900

    package.json fix

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
-  <header class="-header">
-    aaa
+  <header class="main-header">
+    <h1>GIF SEARCH</h1>
   </header>
 </template>
 
-<style style="scss" scoped>
-.-header{
-  height: 60px;
+<style lang="scss" scoped>
+.main-header{
+  height: 64px;
   width: 100%;
+  display: flex;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
+  h1 {
+    font-weight: 700;
+  }
 }
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import axios from 'axios'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
