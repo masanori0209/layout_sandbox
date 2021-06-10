6c63a9b4e05c01e7ad83129c7d81283b3d63e188
masanori0209 <masanori.murakoshi@akarie.co.jp>
2021-06-10 13:59:05 +0900

    install giphy

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import axios from 'axios'
+import { GiphyFetch } from '@giphy/js-fetch-api'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
@@ -12,6 +13,7 @@ Vue.prototype.$axios = axios.create({
   xsrfHeaderName: 'X-CSRFToken',
   'Content-Type': 'application/json'
 })
+Vue.prototype.$gf = new GiphyFetch('your api key')
 
 new Vue({
   router,
