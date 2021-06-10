6fd4eab8defac4c1966062a2c7cdcec359b3b329
masanori0209 <masanori.murakoshi@akarie.co.jp>
2021-06-10 20:03:49 +0900

    fix

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   pages: {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/public/'   //任意
-    : '/'
     index: {
       entry: "src/main.js",
       title: "GIF SEARCH | GIF検索サービス"
